866c3bab78ded914d1bd3ead087a08b8da3c0479
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-10-06 09:56:09 -0400

    Pin country data in recurrent schedule

--- shiny/GIC_of_medians_Gadget/app.R
+++ shiny/GIC_of_medians_Gadget/app.R
@@ -6,7 +6,6 @@ library("shiny")
 library("miniUI")
 library("pins")
 
-board_register_github(repo = "PovcalNet-Team/pov_ineq_chart_book")
 #----------------------------------------------------------
 #   subfunctions
 #----------------------------------------------------------
@@ -14,22 +13,30 @@ board_register_github(repo = "PovcalNet-Team/pov_ineq_chart_book")
 
 
 qtile <- function(x, nq = 10) {
-  N   <-  length(x)
-  csw <-  1:N
-  qp  <-   floor(csw/((N+1)/nq)) + 1
-  return(qp)
+    N   <-  length(x)
+    csw <-  1:N
+    qp  <-   floor(csw/((N+1)/nq)) + 1
+    return(qp)
 }
 
 
 
+#--------- Connect to data ---------
+
+my_key    <- Sys.getenv("connect_key")
+my_server <- "http://w0lxopshyprd1b.worldbank.org:3939/"
+my_server  <- "http://localhost:3939/"
+
+board_register_rsconnect(server = my_server,
+                         key    = my_key)
+
+dfc <- pin_get(name = "deciles_by_country",
+               board = "rsconnect")
 
 
 #----------------------------------------------------------
 #   Calculate Quantiles
 #----------------------------------------------------------
-
-dfc <- pin_get("dfc", board = "github")
-
 
 # set data.table
 
@@ -38,18 +45,18 @@ setnames(DT, "threshold", "pv")
 
 
 DT <- DT[
-  # remove old years
-  year >= 1990
+    # remove old years
+    year >= 1990
 ][
-  # filter negative values (which we should not have)
-  pv > 0 & !is.na(pv)
+    # filter negative values (which we should not have)
+    pv > 0 & !is.na(pv)
 ][,
   # multiply by 100
   goal := 100*goal
 
 ][
-  ,
-  headcount := NULL
+    ,
+    headcount := NULL
 ]
 
 
@@ -57,117 +64,117 @@ yrs <- DT[, unique(year)]
 
 
 ui <- miniPage(
-  miniTitleBar("Growth Incidence Curve of Medians"),
-  miniTabstripPanel(
-    miniTabPanel("Parameters",
-                 icon = icon("sliders"),
-                 miniContentPanel(sliderInput("pc",
-                                              label = "Percentile",
-                                              min = 10, max = 100, value = 50, step = 10),
+    miniTitleBar("Growth Incidence Curve of Medians"),
+    miniTabstripPanel(
+        miniTabPanel("Parameters",
+                     icon = icon("sliders"),
+                     miniContentPanel(sliderInput("pc",
+                                                  label = "Percentile",
+                                                  min = 10, max = 100, value = 50, step = 10),
 
-                                  numericInput("nq",
-                                               label = "Number of quantiles",
-                                               min = 5, max = 100, value = 10, step  = 5),
+                                      numericInput("nq",
+                                                   label = "Number of quantiles",
+                                                   min = 5, max = 100, value = 10, step  = 5),
 
-                                  selectInput("yr1",
-                                              label    = "Year 1",
-                                              choices  = yrs,
-                                              selected = 1993),
+                                      selectInput("yr1",
+                                                  label    = "Year 1",
+                                                  choices  = yrs,
+                                                  selected = 1993),
 
-                                  selectInput("yr2",
-                                              label    = "Year 2",
-                                              choices  = yrs,
-                                              selected = 2017),
+                                      selectInput("yr2",
+                                                  label    = "Year 2",
+                                                  choices  = yrs,
+                                                  selected = 2017),
 
-                                  selectInput("ms",
-                                              label = "Measure:",
-                                              choices = c("sum", "mean", "min", "max"),
-                                              selected = "max")
-                 )
-    ),
-    miniTabPanel("Visualize", icon = icon("area-chart"),
-                 miniContentPanel(plotlyOutput("GIC", height = "100%"))
-    ),
-    miniTabPanel("Data", icon = icon("table"),
-                 miniContentPanel(DT::dataTableOutput("table"))
+                                      selectInput("ms",
+                                                  label = "Measure:",
+                                                  choices = c("sum", "mean", "min", "max"),
+                                                  selected = "max")
+                     )
+        ),
+        miniTabPanel("Visualize", icon = icon("area-chart"),
+                     miniContentPanel(plotlyOutput("GIC", height = "100%"))
+        ),
+        miniTabPanel("Data", icon = icon("table"),
+                     miniContentPanel(DT::dataTableOutput("table"))
+        )
     )
-  )
 ) # End of UI
 
 
 server <- function(input, output, session) {
 
-  DQ <- reactive({
+    DQ <- reactive({
 
-    calc <- paste0(".(", input$ms, "  = ", input$ms, "(pv, na.rm = TRUE))")
-    calc <- parse(text = calc)
+        calc <- paste0(".(", input$ms, "  = ", input$ms, "(pv, na.rm = TRUE))")
+        calc <- parse(text = calc)
 
-    # Make sure the input are included correclty in data.table (not efficient but works)
-    iyr1  <- as.numeric(input$yr1)
-    iyr2  <- as.numeric(input$yr2)
-    yrdif <- iyr2 - iyr1
-    yrst  <- c(iyr1, iyr2)
+        # Make sure the input are included correclty in data.table (not efficient but works)
+        iyr1  <- as.numeric(input$yr1)
+        iyr2  <- as.numeric(input$yr2)
+        yrdif <- iyr2 - iyr1
+        yrst  <- c(iyr1, iyr2)
 
-    DQ <-
-      DT[
-        year  %in% yrst
-        & goal == input$pc
-      ]
+        DQ <-
+            DT[
+                year  %in% yrst
+                & goal == input$pc
+            ]
 
-    setorder(DQ, year, pv)
-    DQ <-
-      DQ[
-        , # Create deciles in each percentile
-        qp := qtile(pv, input$nq),
-        by = .(year)
-      ][
-        , # Make requested calculation
-        eval(calc),
-        by = .(year, qp)
-      ][
-        ,
-        yr := ifelse(year == iyr1, "yr1", "yr2")
-      ]
-
-
-    DQ <- dcast(DQ,
-                formula = qp ~ yr,
-                value.var = input$ms)
-
-    DQ <-
-      DQ[
-        ,
-        gic := ((yr2/yr1)^(1 / yrdif)) - 1
-      ]
-    return(DQ)
-
-  })
+        setorder(DQ, year, pv)
+        DQ <-
+            DQ[
+                , # Create deciles in each percentile
+                qp := qtile(pv, input$nq),
+                by = .(year)
+            ][
+                , # Make requested calculation
+                eval(calc),
+                by = .(year, qp)
+            ][
+                ,
+                yr := ifelse(year == iyr1, "yr1", "yr2")
+            ]
 
 
-  output$GIC <- renderPlotly({
-    pn <- ggplot(DQ(),
-                 aes(
-                   x = qp,
-                   y = gic
-                 )
-    ) +
-      geom_point() +
-      geom_line() +
-      geom_hline(yintercept = 0,
-                 color = "red") +
-      theme_minimal() +
-      scale_y_continuous(labels = scales::percent) +
-      labs(
-        title = "Growth Incidence curve of selected percentile",
-        x     = "Quantiles",
-        y     = "Annualized growth"
-      )
+        DQ <- dcast(DQ,
+                    formula = qp ~ yr,
+                    value.var = input$ms)
 
-    ggplotly(pn)
-  })
-  output$table <- DT::renderDataTable({
-    DQ()
-  })
+        DQ <-
+            DQ[
+                ,
+                gic := ((yr2/yr1)^(1 / yrdif)) - 1
+            ]
+        return(DQ)
+
+    })
+
+
+    output$GIC <- renderPlotly({
+        pn <- ggplot(DQ(),
+                     aes(
+                         x = qp,
+                         y = gic
+                     )
+        ) +
+            geom_point() +
+            geom_line() +
+            geom_hline(yintercept = 0,
+                       color = "red") +
+            theme_minimal() +
+            scale_y_continuous(labels = scales::percent) +
+            labs(
+                title = "Growth Incidence curve of selected percentile",
+                x     = "Quantiles",
+                y     = "Annualized growth"
+            )
+
+        ggplotly(pn)
+    })
+    output$table <- DT::renderDataTable({
+        DQ()
+    })
 
 
 } # End of server
